36ea364b8d4468ab4727e000668f7909f82cc2b8
cone26 <sight41siyah@gmail.com>
2024-02-01 07:21:47 +0900

    clock layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className={'container'}>
-      <div className={'sideMenu'}>side</div>
+      {/*<div className={'sideMenu'}>side</div>*/}
       <div className={'timer'}>
-          <div className={'clock'}></div>
+          <div className={'clock'}>
+              <span className={'remainTime'}>12:25</span>
+          </div>
+          <div className={'buttonContainer'}>
+              <button className={'giveUp'}>Give Up</button>
+              <button className={'pause'}>Pause</button>
+          </div>
       </div>
-      <div className={'content'}>menu screen or todo</div>
+      {/*<div className={'content'}>menu screen or todo</div>*/}
     </div>
   );
 }
