9aaf49c7a0acf38f8d1fd3f8a5260aad8863c438
cone26 <sight41siyah@gmail.com>
2024-02-04 22:17:29 +0900

    fix: set time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     const switchStatus = () => {
         setStatus(!status)
     };
-    const [time, setTime] = useState(DEFAULT_TIME*60);
+    const [time, setTime] = useState('00:00');
     const [minute, setMinute] = useState(Math.floor(time / 60));
     const [second, setSecond] = useState(time % 60);
     const [reset, setReset] = useState(false)
@@ -25,22 +25,29 @@ export default function Home() {
         setResetTimer(DEFAULT_TIME*60)
         setClock(resetTimer)
     }
-    if(!status){
-        setTimeout(()=>{
-            setTime(time-1);
-            setClock(time)
-        },1000)
-    }
+    // if(!status){
+    //     setTimeout(()=>{
+    //         setTime(time-1);
+    //         setClock(time)
+    //     },1000)
+    // }
 
-
+    useEffect(()=>{
+        const minutes = String(new Date(DEFAULT_TIME * 60 * 1000).getMinutes()).padStart(2,'0');
+        const seconds = String(new Date(DEFAULT_TIME * 60 * 1000).getSeconds()).padStart(2,'0');
+        setTime(`${minutes}:${seconds}`)
+    })
+    // const timeSpan = () => {
+    //
+    // }
     return (
         <div className={'container'}>
             {/*<div className={'sideMenu'}>side</div>*/}
             <div className={'timer'}>
                 <div className={'clock'}>
-                  <span className={'remainTime'}>
-                      {`${minute}:${second}`}
-                  </span>
+                    <span className={'remainTime'}>
+                        {time}
+                    </span>
                 </div>
                 <div className={'buttonContainer'}>
                     <FilterStatus status={status} switchStatus={switchStatus} resetTime={resetTime}/>
