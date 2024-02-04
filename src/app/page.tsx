e950c0c6a704c393caecc22640aea1a18e3a0c1b
cone26 <sight41siyah@gmail.com>
2024-02-04 22:24:30 +0900

    edit: changed the way how to set time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,35 +8,26 @@ export default function Home() {
         setStatus(!status)
     };
     const [time, setTime] = useState('00:00');
-    const [minute, setMinute] = useState(Math.floor(time / 60));
-    const [second, setSecond] = useState(time % 60);
+    const [initTime, setInitTime] = useState(DEFAULT_TIME * 60 * 1000);
     const [reset, setReset] = useState(false)
     const resetTime = () => {
         setReset(true)
     }
     const [resetTimer, setResetTimer] = useState(0);
 
-    const setClock = (time) => {
-        setMinute(Math.floor(time / 60))
-        setSecond(time % 60)
-    }
-    // 여기 수정
-    if(reset) {
-        setResetTimer(DEFAULT_TIME*60)
-        setClock(resetTimer)
-    }
-    // if(!status){
-    //     setTimeout(()=>{
-    //         setTime(time-1);
-    //         setClock(time)
-    //     },1000)
-    // }
 
+    if(!status){
+        setTimeout(()=>{
+            setInitTime(initTime-1000)
+        },1000)
+    }
+
+    // 느려 !
     useEffect(()=>{
-        const minutes = String(new Date(DEFAULT_TIME * 60 * 1000).getMinutes()).padStart(2,'0');
-        const seconds = String(new Date(DEFAULT_TIME * 60 * 1000).getSeconds()).padStart(2,'0');
+        const minutes = String(new Date(initTime).getMinutes()).padStart(2,'0');
+        const seconds = String(new Date(initTime).getSeconds()).padStart(2,'0');
         setTime(`${minutes}:${seconds}`)
-    })
+    },[initTime])
     // const timeSpan = () => {
     //
     // }
