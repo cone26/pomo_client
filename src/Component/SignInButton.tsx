776da8e43b59e899fb4ae3e98ca3b63213edd93b
cone26 <sight41siyah@gmail.com>
2024-02-13 11:42:00 +0900

    refact: clean

--- src/Component/SignInButton.tsx
+++ src/Component/SignInButton.tsx
@@ -4,9 +4,7 @@ import {signIn, signOut, useSession} from "next-auth/react";
 
 function SignInButton() {
     const { data: session } = useSession();
-    // const handleSubmit = () => {
-    //     alert(`This feature is not ready yet.`);
-    // }
+
     if(session && session.user) {
         return (
             <button
