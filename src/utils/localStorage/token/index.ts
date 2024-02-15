17a23c9d08a9687b9beb5e6bbe8531b9387aa465
cone26 <sight41siyah@gmail.com>
2024-02-15 11:44:59 +0900

    add : localstorage token set

--- src/utils/localStorage/token/index.ts
+++ src/utils/localStorage/token/index.ts
@@ -1,9 +1,18 @@
-import {getLocalStorage} from "@/utils/localStorage/helper";
+import {getLocalStorage, removeLocalStorage, setLocalStorage} from "@/utils/localStorage/helper";
+import {AccessTokenType} from "@/types/Auth.type";
 
-const TOKEN_KEY = process.env.NEXTAUTH_SECRET;
+const TOKEN_KEY = process.env.NEXTAUTH_SECRET || '@token';
 
 export const getToken = () => {
     return getLocalStorage<AccessTokenType>(TOKEN_KEY, {
         accessToken: null,
     })
+}
+
+export const setToken = (token: AccessTokenType) => {
+    setLocalStorage(TOKEN_KEY, token);
+}
+
+export const deleteToken = () => {
+    removeLocalStorage(TOKEN_KEY)
 }
