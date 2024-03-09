7ab934f66643f793c6a2eeb3cf5819eb55d31197
cone26 <sight41siyah@gmail.com>
2024-03-09 21:19:10 +0900

    clean

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,6 @@
 import NextAuth from "next-auth/next";
 import CredentialProvider from 'next-auth/providers/credentials'
 import {CONFIG} from "../../../../../config";
-import {setToken} from "@/utils/localStorage/token";
-import {useSession} from "next-auth/react";
-import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
     providers: [
