b39ba06127cc70860ef378093000eb3dabfca2d4
cone26 <sight41siyah@gmail.com>
2024-03-10 23:31:32 +0900

    comment

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,27 +1,27 @@
-import {setToken} from "@/utils/localStorage/token";
-import {CONFIG} from "../../../../config";
-
-interface RequestBody {
-    email: string;
-    password: string;
-}
-
-export async function POST(request: Request) {
-    const body: RequestBody = await request.json()
-
-    const res = await fetch(`${CONFIG.SERVER}/auth/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            email: body?.email,
-            password: body?.password,
-        }),
-    });
-    // const user = await res.json()
-    // setToken({
-    //     accessToken: user.data.accessToken
-    // })
-    return new Response(JSON.stringify(res))
-}
+// import {setToken} from "@/utils/localStorage/token";
+// import {CONFIG} from "../../../../config";
+//
+// interface RequestBody {
+//     email: string;
+//     password: string;
+// }
+//
+// export async function POST(request: Request) {
+//     const body: RequestBody = await request.json()
+//
+//     const res = await fetch(`${CONFIG.SERVER}/auth/login`, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//             email: body?.email,
+//             password: body?.password,
+//         }),
+//     });
+//     // const user = await res.json()
+//     // setToken({
+//     //     accessToken: user.data.accessToken
+//     // })
+//     return new Response(JSON.stringify(res))
+// }
